6ab73980900a6291e0bdc01f5c8b4362033e7753
DreamGuy's <dreamguystech@gmail.com>
2016-09-27 14:34:43 +0530

    updated

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@ document.addEventListener("touchstart", function(){}, true);
 
 $(function() {
     'use strict';
-	$(".profile-icon").append(window.localStorage.getItem("prof_img"));
+
     var dataSplash = $('.page-content').attr('data-splash');
     var dataRedirect = $('.page-content').attr('data-redirect');
     if(dataSplash>0){
@@ -84,4 +84,12 @@ $(function() {
     window.onpopstate = function(e){
         $('.loading-mask').addClass('stop-loading');
     };
+	
+	
 });
+
+  /*$(document).ready(function() {
+	$("#news-list").endlessRiver({
+        buttons: true
+    });
+  });*/
